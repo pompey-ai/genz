e27b36b6effdd7785ac5d3da60d129bc615a2888
HrGoutham <hrgoutham678@gmail.com>
2022-09-08 15:16:03 +0530

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './Modules/landing-page/landing-page.component';
 import { DashboardComponent } from './Modules/dashboard/dashboard.component';
+import { LoginComponent } from './Modules/authentication/login/login.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    DashboardComponent
+    DashboardComponent,LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,MatFormFieldModule,MatInputModule
+    
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
